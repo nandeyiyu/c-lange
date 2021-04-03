cd498bc6c29625928a43fc3d6aa7932ac6d46d4d
nandeyiyu <2726974724@qq.com>
2021-04-03 14:51:34 +0800

    测试GitHub

---
+++ 自学1/自学1/test2020317.cpp
@@ -0,0 +1,20 @@
+
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include<string.h>
+
+
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？（1/0）>;");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//	return 0;
+//
+//}
